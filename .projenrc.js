74437c2a2793298e4a491f7bd92afd255feec1b5
Mykhailo Yaremtso <mykhailo.yaremtso@globallogic.com>
2025-04-08 21:05:46 +0300

    Add build with Node.js v18.12.0 and projen version 0.87.4, terraform openstack version 3.0

--- .projenrc.js
+++ .projenrc.js
@@ -8,7 +8,7 @@ const { CdktfProviderProject } = require("@cdktf/provider-project");
 const typescriptVersion = "~5.4.0";
 const project = new CdktfProviderProject({
   useCustomGithubRunner: false,
-  terraformProvider: "terraform-provider-openstack/openstack@~> 3.0.0",
+  terraformProvider: "terraform-provider-openstack/openstack@~> 3.0",
   cdktfVersion: "^0.20.0",
   constructsVersion: "^10.3.0",
   minNodeVersion: "18.12.0",
